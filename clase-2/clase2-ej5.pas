610f8c160fbb53e34d4aee5c797586c260da3b1a
daniNKV <nikiforovdaniel@gmail.com>
2022-09-05 00:32:52 -0300

    Clase 2 - Ejercicio 5

--- clase-2/clase2-ej5.pas
+++ clase-2/clase2-ej5.pas
@@ -4,3 +4,34 @@
 
 // Nota: El parámetro “pos” debe retornar la posición del dato o -1 si el dato no se encuentra
 // en el vector.
+
+program ej5;
+
+const
+
+type
+
+
+Procedure busquedaDicotomica (v: vector; ini,fin: indice; dato:integer; var pos: indice);
+var
+    elementoMedio: indice;
+
+begin
+    elementoMedio := fin DIV 2;
+    if (v[elementoMedio] <> dato) then begin
+        if dato > v[elementoMedio] then
+            busquedaDicotomica(v, elementoMedio, fin, dato, pos);
+        else
+            busquedaDicotomica(v, ini, elementoMedio, dato, pos);
+    end
+    else 
+        pos := fin DIV 2;
+
+end;
+
+
+
+var
+begin
+
+end.

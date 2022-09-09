ac5352c060839b20c887393f0c8ad40cc69f4605
daniNKV <nikiforovdaniel@gmail.com>
2022-09-09 11:46:28 -0300

    Clase 4, ejercicio 2 inciso A: generando vector de listas

--- clase-4/clase4-ej2.pas
+++ clase-4/clase4-ej2.pas
@@ -7,3 +7,105 @@
 // cuando se lee el código de película -1.
 // b. Un módulo que reciba la estructura generada en el punto a y retorne una estructura de
 // datos donde estén todas las películas almacenadas ordenadas por código de película.
+
+program ej2;
+const
+    cantidadGeneros = 8;
+    puntajeMaximo = 10;
+    puntajeMinimo = 0;
+
+type
+    rangoPuntaje = puntajeMinimo .. puntajeMaximo;
+    rangoGeneros = 1 .. cantidadGeneros;
+
+    pelicula = record
+        codigo: integer;
+        genero: rangoGeneros;
+        puntaje: rangoPuntaje;
+    end;
+
+    listaPeliculas = ^nodoPelicula
+    
+    nodoPelicula = record
+        dato: pelicula;
+        sig: listaPeliculas;
+    end;
+
+    vectorGeneros = array[rangoGeneros] of listaPeliculas;
+
+
+
+procedure generarVectorDeListas(var v: vectorGeneros);
+    procedure leerPelicula(var p: pelicula);
+    begin
+        write('Ingrese codigo de pelicula: ');
+        readln(p.codigo);
+        if (p.codigo <> -1) then begin
+            write('Ingrese genero de pelicula: ');
+            readln(p.genero);
+            write('Ingrese puntaje dado por la crítica: ');
+            readln(p.puntaje);
+        end;
+    end;
+
+    procedure iniciarListas(var v: vectorGeneros);
+    var
+        i: integer;
+    begin
+        for i := 1 to cantidadGeneros do
+            v[i] := nil;
+    end;
+
+    procedure agregarPelicula(var v:vectorGeneros; p: pelicula);
+        procedure insertarOrdenado(var pi: listaPeliculas; p: pelicula);
+        var
+            ant, act: listaPeliculas;
+            puntero: listaPeliculas;
+            nuevoNodo: listaPeliculas; 
+        begin
+            puntero := pi;
+            new(nuevoNodo);
+            nuevoNodo^.dato := p;
+            nuevoNodo^.sig := nil;
+
+            if (pi = nil) then
+                pi := nuevoNodo;
+            else
+                while (p.codigo > puntero^.dato.codigo) and (puntero^.sig <> nil) do begin
+                    act := puntero;
+                    puntero := puntero^.sig;
+                end;
+                
+                nuevoNodo^.sig := act^.sig;
+                act^.sig := nuevoNodo;
+            
+        end;
+    var
+        punteroGenero: listaPeliculas;
+    begin
+        punteroGenero := v[p.codigo];
+
+        insertarOrdenado(punteroGenero, p);
+
+
+    end;
+var
+    peliculaNueva: pelicula;
+begin
+    iniciarListas(peliculasPorGenero);
+
+    leerPelicula(peliculaNueva);
+    while (peliculaNueva.codigo <> -1) do begin
+        agregarPelicula(v, peliculaNueva);
+        leerPelicula(peliculaNueva);
+    end;
+end;
+
+
+
+var
+    peliculasPorGenero: vectorGeneros;
+begin
+
+
+end.

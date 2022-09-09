a9a6cdf51792a2eaff9765ccbea7d61e07125c25
daniNKV <nikiforovdaniel@gmail.com>
2022-09-09 17:16:27 -0300

    Clase 4: ejercicio 3 A y B

--- clase-4/ImperativoClase4Merge.pas
+++ clase-4/ImperativoClase4Merge.pas
@@ -13,227 +13,233 @@ f.	Un módulo recursivo que reciba la estructura generada en e. y muestre su con
 
 Program ejer1Clase4;
 Const dimF = 12;
-      valorGrande = 9999;
-Type rangoDias = 1.. 31;
-     rangoMeses = 1..12;
-     prestamo = record
-                  ISBN: integer;
-                  numeroSocio: integer;
-                  dia: rangoDias;
-                  mes: rangoMeses;
-                  cantDias: integer;
-                end;
-     prestamoAGuardar = record    // NO contiene el mes
-                          ISBN: integer;
-                          numeroSocio: integer;
-                          dia: rangoDias;
-                          cantDias: integer;
-                        end;
-     lista = ^nodo;
-     nodo = record
-              dato: prestamoAGuardar;
-              sig: lista;
-            end;
-     vector = array [1..dimF] of lista;
-     elemAcumulador = record    
-                        ISBN: integer;
-                        cantPrestamos: integer;
-                      end;
-     listaAcumulador = ^nodoAcumulador;
-     nodoAcumulador = record
-                         dato: elemAcumulador;
-                         sig: listaAcumulador;
-                      end;
+			valorGrande = 9999;
+Type 
+		rangoDias = 1.. 31;
+		rangoMeses = 1..12;
+		prestamo = record
+			ISBN: integer;
+			numeroSocio: integer;
+			dia: rangoDias;
+			mes: rangoMeses;
+			cantDias: integer;
+		end;
+		prestamoAGuardar = record    // NO contiene el mes
+			ISBN: integer;
+			numeroSocio: integer;
+			dia: rangoDias;
+			cantDias: integer;
+		end;
+		lista = ^nodo;
+		nodo = record
+			dato: prestamoAGuardar;
+			sig: lista;
+		end;
+		vector = array [1..dimF] of lista;
+		 elemAcumulador = record    
+												ISBN: integer;
+												cantPrestamos: integer;
+											end;
+		 listaAcumulador = ^nodoAcumulador;
+		 nodoAcumulador = record
+												 dato: elemAcumulador;
+												 sig: listaAcumulador;
+											end;
 
 
 procedure GenerarVectorListas (var v: vector);
-   
-   procedure CrearListasEnVector (var v: vector);
-   var i: integer;
-   begin
-     for i:= 1 to dimF do
-        v [i]:= nil
-   end;
-   
-   procedure LeerPrestamo (var p: prestamo);
-   begin
-     write ('Ingrese ISBN: ');
-     readln (p.ISBN);
-     If (p.ISBN <> -1)
-     then begin
-            write ('Ingrese numero de socio: ');
-            p.numeroSocio:= Random (100) + 100;
-            writeln (p.numeroSocio);
-            write ('Ingrese dia: ');
-            p.dia:= Random (30) + 1;
-            writeln (p.dia);
-            write ('Ingrese mes: ');
-            readln (p.mes);
-            write ('Ingrese cantidad: ');
-            p.cantDias:= Random (14) + 1;
-            writeln (p.cantDias);
-           end;
-   end;
+	 
+	 procedure CrearListasEnVector (var v: vector);
+	 var i: integer;
+	 begin
+		 for i:= 1 to dimF do
+				v [i]:= nil
+	 end;
+	 
+	 procedure LeerPrestamo (var p: prestamo);
+	 begin
+		 write ('Ingrese ISBN: ');
+		 readln (p.ISBN);
+		 If (p.ISBN <> -1)
+		 then begin
+						write ('Ingrese numero de socio: ');
+						p.numeroSocio:= Random (100) + 100;
+						writeln (p.numeroSocio);
+						write ('Ingrese dia: ');
+						p.dia:= Random (30) + 1;
+						writeln (p.dia);
+						write ('Ingrese mes: ');
+						readln (p.mes);
+						write ('Ingrese cantidad: ');
+						p.cantDias:= Random (14) + 1;
+						writeln (p.cantDias);
+					 end;
+	 end;
 
-   procedure InsertarElementoEnLista(var l: Lista; pG: prestamoAGuardar);
-   var ant, nuevo, act: lista;
-   begin
-     new (nuevo);
-     nuevo^.dato := pG;
-     act := l;
-     {Recorro mientras no se termine la lista y no encuentro la posición correcta}
-     while (act <> NIL) and (act^.dato.ISBN < pG.ISBN) do begin
-        ant := act;
-        act := act^.sig ;
-     end;
-     if (act = l)  then l:= nuevo   {el dato va al principio}
-                   else ant^.sig  := nuevo; {va entre otros dos o al final}
-     nuevo^.sig := act ;
-   end;
+	 procedure InsertarElementoEnLista(var l: Lista; pG: prestamoAGuardar);
+	 var ant, nuevo, act: lista;
+	 begin
+		 new (nuevo);
+		 nuevo^.dato := pG;
+		 act := l;
+		 {Recorro mientras no se termine la lista y no encuentro la posición correcta}
+		 while (act <> NIL) and (act^.dato.ISBN < pG.ISBN) do begin
+				ant := act;
+				act := act^.sig ;
+		 end;
+		 if (act = l)  then 
+		 	l:= nuevo   {el dato va al principio}
+		else ant^.sig  := nuevo; {va entre otros dos o al final}
+			nuevo^.sig := act ;
+	 end;
 
-   
-   procedure ArmarPrestamoAguardar (p: prestamo; var pG: prestamoAGuardar);
-   begin
-     pG.ISBN:= p.ISBN;
-     pG.numeroSocio:= p.numeroSocio;
-     pG.dia:= p.dia;
-     pG.cantDias:= p.cantDias;
-   end;
+	 
+	 procedure ArmarPrestamoAguardar (p: prestamo; var pG: prestamoAGuardar);
+	 begin
+		 pG.ISBN:= p.ISBN;
+		 pG.numeroSocio:= p.numeroSocio;
+		 pG.dia:= p.dia;
+		 pG.cantDias:= p.cantDias;
+	 end;
 
 var p: prestamo; pG: prestamoAGuardar;
 begin
-  CrearListasEnVector (v);
-  LeerPrestamo (p);
-   while (p.ISBN <> -1) do 
-   begin
-     ArmarPrestamoAguardar (p, pG);
-     InsertarElementoEnLista (v[p.mes], pG);
-     LeerPrestamo (p);
-   end;
+	CrearListasEnVector (v);
+	LeerPrestamo (p);
+	 while (p.ISBN <> -1) do 
+	 begin
+		 ArmarPrestamoAguardar (p, pG);
+		 InsertarElementoEnLista (v[p.mes], pG);
+		 LeerPrestamo (p);
+	 end;
 end;
 
 procedure imprimirLista(l: lista);
-  begin
-     if (l <> nil) then 
-     begin
-        writeln('      ISBN: ', l^.dato.ISBN, '  Numero de socio: ', l^.dato.numeroSocio);
-        imprimirLista (l^.sig);
-     end;
-  end;
+	begin
+		 if (l <> nil) then 
+		 begin
+				writeln('      ISBN: ', l^.dato.ISBN, '  Numero de socio: ', l^.dato.numeroSocio);
+				imprimirLista (l^.sig);
+		 end;
+	end;
 
 procedure ImprimirVectorListas (v: vector);
 var i: integer;
 begin
-  writeln;
-  writeln ('----- Vector de listas ----->');
-  writeln;
-  for i:= 1 to dimF do
-  begin
-     writeln;
-     writeln ('Mes ', i);
-     if (v[i] = nil ) 
-     then writeln ('      Sin elementos')
-     else imprimirLista (v[i]);
-  end;
+	writeln;
+	writeln ('----- Vector de listas ----->');
+	writeln;
+	for i:= 1 to dimF do
+	begin
+		 writeln;
+		 writeln ('Mes ', i);
+		 if (v[i] = nil ) 
+		 then writeln ('      Sin elementos')
+		 else imprimirLista (v[i]);
+	end;
 end;
-  
+	
 Procedure minimo (var v: vector; var prestamoMin: prestamoAGuardar);
-   var indiceMin,i:integer;
-   Begin
-     prestamoMin.ISBN:= valorGrande;
-     for i:= 1 to dimF do
-     begin
-       if (v[i] <> nil) 
-       then if (v [i]^.dato.ISBN <= prestamoMin.ISBN) 
-            then begin
-                   indiceMin:= i;
-                   prestamoMin:= v [i]^.dato;  
-                 end; 
-     end;
-     if (prestamoMin.ISBN <> valorGrande) 
-     then v[indiceMin]:= v[indiceMin]^.sig;
-   end;
-   
+	 var indiceMin,i:integer;
+	 Begin
+		 prestamoMin.ISBN:= valorGrande;
+		 for i:= 1 to dimF do
+		 begin
+			 if (v[i] <> nil) 
+			 then if (v [i]^.dato.ISBN <= prestamoMin.ISBN) 
+						then begin
+									 indiceMin:= i;
+									 prestamoMin:= v [i]^.dato;  
+								 end; 
+		 end;
+		 if (prestamoMin.ISBN <> valorGrande) 
+		 then v[indiceMin]:= v[indiceMin]^.sig;
+	 end;
+	 
  procedure Merge (v: vector; var l: lista);
-   
-   procedure AgregarAtras (var l, ult: lista; p: prestamoAGuardar); 
-   var  nuevo: lista;
-   begin 
-     new (nuevo); nuevo^.dato:= p; nuevo^.sig := nil;
-     if (l = nil) then l:= nuevo
-                  else ult^.sig := nuevo;
-     ult := nuevo;
-   end;
+	 
+	 procedure AgregarAtras (var l, ult: lista; p: prestamoAGuardar); 
+	 var  nuevo: lista;
+	 begin 
+		new (nuevo);
+		nuevo^.dato:= p; 
+		nuevo^.sig := nil;
+		if (l = nil) then 
+			l:= nuevo
+		else 
+			ult^.sig := nuevo;
+			ult := nuevo;
+	 end;
 
 
 var prestamoMin: prestamoAGuardar; ult: lista;
 begin
-  l:= nil;
-  minimo (v, prestamoMin);
-  while (prestamoMin.ISBN <> valorGrande) do
-  begin
-    agregarAtras(l, ult, prestamoMin);
-    minimo (v, prestamoMin);
-  end;
+	l:= nil;
+	minimo (v, prestamoMin);
+	while (prestamoMin.ISBN <> valorGrande) do
+	begin
+		agregarAtras(l, ult, prestamoMin);
+		minimo (v, prestamoMin);
+	end;
 end;  
 
 procedure imprimirEstructuraMerge (l: lista);
 begin
-  writeln;
-  writeln ('----- Estructura merge ----->');
-  writeln;
-  if (l = nil ) 
-     then writeln ('      Sin elementos')
-     else imprimirLista (l);
+	writeln;
+	writeln ('----- Estructura merge ----->');
+	writeln;
+	if (l = nil ) 
+		 then writeln ('      Sin elementos')
+		 else imprimirLista (l);
 end;
 
 procedure MergeAcumulador (v: vector; var l: listaAcumulador);
 
-   procedure agregarAtras2 (var l, ult: listaAcumulador; e: elemAcumulador); 
-   var  nuevo: listaAcumulador;
-   begin 
-     new (nuevo); nuevo^.dato:= e; nuevo^.sig := nil;
-     if (l = nil) then l:= nuevo
-                  else ult^.sig := nuevo;
-     ult := nuevo;
-   end;
+	 procedure agregarAtras2 (var l, ult: listaAcumulador; e: elemAcumulador); 
+	 var  nuevo: listaAcumulador;
+	 begin 
+		 new (nuevo); nuevo^.dato:= e; nuevo^.sig := nil;
+		 if (l = nil) then l:= nuevo
+									else ult^.sig := nuevo;
+		 ult := nuevo;
+	 end;
 
 var prestamoMin: prestamoAGuardar; 
-    ult: listaAcumulador;
-    actual: elemAcumulador;
+		ult: listaAcumulador;
+		actual: elemAcumulador;
 begin
-  l:= nil;
-  minimo (v, prestamoMin);
-  while (prestamoMin.ISBN <> valorGrande) do
-  begin
-    actual.ISBN:= prestamoMin.ISBN; actual.cantPrestamos:= 0;
-    while (actual.ISBN = prestamoMin.ISBN) do
-    begin
-       actual.cantPrestamos:= actual.cantPrestamos + 1;
-       minimo (v, prestamoMin);
-    end;
-    agregarAtras2 (l, ult, actual);
-  end;
+	l:= nil;
+	minimo (v, prestamoMin);
+	while (prestamoMin.ISBN <> valorGrande) do
+	begin
+		actual.ISBN:= prestamoMin.ISBN; actual.cantPrestamos:= 0;
+		while (actual.ISBN = prestamoMin.ISBN) do
+		begin
+			 actual.cantPrestamos:= actual.cantPrestamos + 1;
+			 minimo (v, prestamoMin);
+		end;
+		agregarAtras2 (l, ult, actual);
+	end;
 end;
 
 procedure ImprimirEstructuraMergeAcumulador (l: listaAcumulador);
 
-  procedure imprimirListaAcumulador (l: listaAcumulador);
-  begin
-     if (l <> nil) then 
-     begin
-        writeln('      ISBN: ', l^.dato.ISBN, '  Cantidad prestamos: ', l^.dato.cantPrestamos);
-        imprimirListaAcumulador (l^.sig);
-     end;
-  end;
-  
+	procedure imprimirListaAcumulador (l: listaAcumulador);
+	begin
+		 if (l <> nil) then 
+		 begin
+				writeln('      ISBN: ', l^.dato.ISBN, '  Cantidad prestamos: ', l^.dato.cantPrestamos);
+				imprimirListaAcumulador (l^.sig);
+		 end;
+	end;
+	
 begin
-  writeln;
-  writeln ('----- Estructura merge acumulador ----->');
-  writeln;
-  if (l = nil ) 
-     then writeln ('      Sin elementos')
-     else imprimirListaAcumulador (l);
+	writeln;
+	writeln ('----- Estructura merge acumulador ----->');
+	writeln;
+	if (l = nil ) 
+		 then writeln ('      Sin elementos')
+		 else imprimirListaAcumulador (l);
 end;
 
 var v: vector; l1: lista; l2: listaAcumulador;
--- clase-4/clase4-ej3.pas
+++ clase-4/clase4-ej3.pas
@@ -6,3 +6,162 @@
 // estructura de datos adecuada.
 // b. Contenga un módulo que reciba la estructura generada en el punto a y retorne una
 // estructura donde esté acumulada la cantidad total vendida para cada código de producto.
+
+program ej3;
+const
+    cantidadSucursales = 4;
+type
+    rangoSucursales 0 .. cantidadSucursales;
+
+    venta = record
+        fecha: string;
+        codigoProducto: integer;
+        codigoSucursal: integer;
+        cantidadVendida: integer;
+    end;
+
+    listaVentas = ^nodoVenta;
+    
+    nodoVenta = record
+        dato: venta;
+        sig: listaVentas;
+    end;
+
+    vectorVentas = array[rangoSucursales] of listaVentas;
+    
+    ventasTotales = record
+        codigoProducto: integer;
+        cantidadVendida: integer;
+    end;
+
+    listaVentasTotales = ^nodoTotal;
+
+    nodoTotal = record
+        dato: ventasTotales;
+        sig: listaVentasTotales;
+
+    end;
+
+
+procedure generarListasDeVentas(var v: vectorVentas);
+    procedure iniciarListas(var v: vectorVentas);
+    var i: integer;
+    begin
+        for i := 0 to cantidadSucursales do 
+            v[i] := nil;
+    end;
+    procedure leerVenta(var v: venta);
+    begin
+        write('Ingrese sucursal donde se vendió el producto: ');
+        readln(v.codigoSucursal);
+        if(v.codigo <> 0) then begin
+            write('Ingrese fecha de venta: ');
+            readln(v.fecha);
+            write('Ingrese codigo del producto vendido:  ');
+            readln(v.codigoProducto);
+            write('Ingrese cantidad vendida: ');
+            readln(v.cantidadVendida);
+    end;
+    procedure insertarOrdenado(pi: listaVentas; v: venta);
+    var
+        nuevoNodo: listaVentas;
+        ant, act: listaVentas;
+    begin
+        new(nuevoNodo);
+        nuevoNodo^.dato := v;
+        nuevoNodo^.sig := nil;
+        act := pi;
+
+        while (act <> nil) and (v.codigoProducto > act^.dato.codigoProducto) do begin
+            ant := act;
+            act := act^.sig;
+        end;
+
+        if (act = nil) then begin
+            pi := nuevoNodo;
+        else 
+            ant^.sig := nuevoNodo;
+            nuevoNodo^.sig := act;
+        end;
+    end;
+
+var
+    nuevaVenta: venta;
+begin
+    iniciarListas(v);
+    leerVenta(nuevaVenta);
+    while(nuevaVenta.codigoSucursal <> -1) do begin
+        insertarOrdenado(v[nuevaVenta.codigoSucursal], nuevaVenta);
+        leerVenta(nuevaVenta);
+    end;
+end;
+
+procedure generarListasDeVentasTotales(v: vectorVentas; var pi: listaVentasTotales);
+    procedure insertarAtras(var pi, pf: listaVentasTotales; cod, total: integer);
+    var
+        nuevo: nodoTotal;
+        act: listaVentasTotales;
+    begin
+        new(nuevo);
+        nuevo^.dato.codigoProducto := cod;
+        nuevo^.dato.cantidadVendida := total;
+        nuevo^.sig := nil;
+        act := pi;
+        if (pi = nil) then
+            pi := nuevo;
+        else
+            pf^.sig := nuevo;
+            nuevo := pf;
+    end;
+
+    procedure buscarMinimo(var v: vectorVentas; var codMin, cantVentas: integer);
+    var 
+        i: integer;
+        indiceMin: integer;
+    begin
+        codMin := 9999;
+        for i := 1 to cantidadSucursales do begin
+            if (v[i] <> nil) then
+                if (v[i]^.dato.codigoProducto < codMin) then begin
+                    cantVentas := v[i]^.dato.cantidadVendida;
+                    codMin := v[i]^.dato.codigoProducto;
+                    indiceMin := i;
+                end;
+        end;
+
+        if (codMin <> 9999) then begin
+            v[indiceMin] := v[indiceMin]^.sig;
+        end;
+    end; 
+var
+    pf: listaVentasTotales;
+    ventaCodMin: venta;
+    codActual: integer;
+    total: integer;
+    codMin: integer,
+    cantVentasMin: integer;
+begin
+    pf := nil;
+    pi := nil;
+
+    buscarMinimo(v, codMin, cantVentasMin);
+
+    
+    while (codMin <> 9999) do begin
+        codActual := codMin;
+        total := 0;
+        while (codMin <> 9999) and (codMin = codActual) do begin
+            total := total + cantVentasMin;
+            buscarMinimo(v, codMin, cantVentasMin);
+        end;
+        insertarAtras(pi, pf, codActual, total);
+    end;
+
+end;
+var
+    listasDeVentas: vectorVentas;
+    totalVentas: listaVentasTotales;
+begin
+    generarListasDeVentas(listasDeVentas);
+    generarListasDeVentasTotales(totalVentas);
+end.

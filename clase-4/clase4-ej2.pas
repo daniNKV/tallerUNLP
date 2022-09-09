9124cfe05ed8825ad8721616471246dd07f1a1c0
daniNKV <nikiforovdaniel@gmail.com>
2022-09-09 10:42:02 -0300

    Iniciando práctica 4

---
+++ clase-4/clase4-ej2.pas
@@ -0,0 +1,9 @@
+// 2. Un cine posee la lista de películas que proyectará durante el mes de octubre. De cada
+// película se conoce: código de película, código de género (1: acción, 2: aventura, 3: drama, 4:
+// suspenso, 5: comedia, 6: bélica, 7: documental y 8: terror) y puntaje promedio otorgado por
+// las críticas. Implementar un programa que contenga:
+// a. Un módulo que lea los datos de películas y los almacene ordenados por código de película y
+// agrupados por código de género, en una estructura de datos adecuada. La lectura finaliza
+// cuando se lee el código de película -1.
+// b. Un módulo que reciba la estructura generada en el punto a y retorne una estructura de
+// datos donde estén todas las películas almacenadas ordenadas por código de película.
---
+++ clase-4/clase4-ej3.pas
@@ -0,0 +1,8 @@
+// 3. Implementar un programa que procese la información de las ventas de productos de una
+// librería que tiene 4 sucursales. De cada venta se lee fecha de venta, código del producto
+// vendido, código de sucursal y cantidad vendida. El ingreso de las ventas finaliza cuando se lee
+// el código de sucursal 0. Implementar un programa que:
+// a. Almacene las ventas ordenadas por código de producto y agrupados por sucursal, en una
+// estructura de datos adecuada.
+// b. Contenga un módulo que reciba la estructura generada en el punto a y retorne una
+// estructura donde esté acumulada la cantidad total vendida para cada código de producto.

926fc845ff1a69334d5d0ae34546b76687b5a9ad
daniNKV <nikiforovdaniel@gmail.com>
2022-09-09 17:50:48 -0300

    Clase 4: ejercicio 4: planteo

--- clase-4/clase4-ej4.pas
+++ clase-4/clase4-ej4.pas
@@ -8,9 +8,72 @@
 // debe estar ordenada por código de obra de forma ascendente.
 
 program ej4;
+const diasSemana = 7;
 
 type
-    funcion = record
-
+    entrada = record
+        dia: rangoDias;
+        codigoObra: integer;
+        asiento: integer;
+        monto: real;
     end;
-    vectorFunciones = array[] of funcion;
+    
+    listaEntradasVendidas = ^nodoEntradas;
+    
+    nodoEntradas = record
+        dato: entrada;
+        sig: listaEntradasVendidas;
+    end;
+
+    rangoDias = 0 .. diasSemana;
+    
+    vectorFunciones = array[rangoDias] of listaEntradasVendidas;
+
+procedure generarListaEntradasVendidas(var pi: listaEntradasVendidas);
+    procedure leerEntrada(var e: entrada);
+    begin
+        write('Ingrese codigo de obra de la funcion: ');
+        readln(e.codigoObra);
+        if(e.codigoObra <> -1) then begin
+            write('Ingrese dia de la funcion: ');
+            readln(e.dia);
+            write('Ingrese asiento: ');
+            readln(e.asiento);
+            write('Ingrese monto abonado: ');
+            readln(e.monto);
+        end;
+    end;
+
+    procedure insertarOrdenado(var pi: listaEntradasVendidas; e: entrada);
+    var
+        nuevo: listaEntradasVendidas;
+        act, ant: listaEntradasVendidas;
+    begin
+        new(nuevo);
+        nuevo^.dato := e;
+        nuevo^.sig := nil;
+        
+        act := pi;
+
+        while (act <> nil) do begin
+            ant := act;
+            act := act^.sig;
+        end;
+
+        if (act = nil) then
+            pi := nuevo;
+        else
+            ant^.sig := nuevo;
+            nuevo^.sig := act;
+    end;
+
+
+begin
+
+end;
+
+
+var
+begin
+
+end.

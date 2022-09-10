5d70e5bf1d9e334c68fbdca32213a20a5ef2b7b8
daniNKV <nikiforovdaniel@gmail.com>
2022-09-10 17:04:23 -0300

    Clase 4: Ejercicio 4 A y B

--- clase-4/clase4-ej4.pas
+++ clase-4/clase4-ej4.pas
@@ -25,11 +25,32 @@ type
         sig: listaEntradasVendidas;
     end;
 
-    rangoDias = 0 .. diasSemana;
+    entradasVendidasObra = record
+        codigoObra: integer;
+        cantidadVendida: integer
+    end;
+
+    listaTotalObra = ^nodoTotal;
+
+    nodoTotal = record;
+        dato: entradasVendidasObra;
+        sig: listaTotalObra;
+    end;
+
+    rangoDias = 0 .. diasSemana;record
+record
+record
+record
     
     vectorFunciones = array[rangoDias] of listaEntradasVendidas;
 
 procedure generarListaEntradasVendidas(var pi: listaEntradasVendidas);
+    procedure iniciarListas(var v: vectorFunciones);
+    var i: integer;
+    begin
+        for i := 1 to diasSemana do begin
+            v[i] := nil;
+    end;
     procedure leerEntrada(var e: entrada);
     begin
         write('Ingrese codigo de obra de la funcion: ');
@@ -43,7 +64,6 @@ procedure generarListaEntradasVendidas(var pi: listaEntradasVendidas);
             readln(e.monto);
         end;
     end;
-
     procedure insertarOrdenado(var pi: listaEntradasVendidas; e: entrada);
     var
         nuevo: listaEntradasVendidas;
@@ -67,13 +87,89 @@ procedure generarListaEntradasVendidas(var pi: listaEntradasVendidas);
             nuevo^.sig := act;
     end;
 
+var
+    entradaLeida: entrada;
 
 begin
+    pi := niL;
 
+    leerEntrada(entradaLeida);
+
+    while (entradaLeida.codigoObra <> 0) do begin
+        insertarOrdenado(pi, entradaLeida);
+        leerEntrada(entradaLeida);
+    
+end;
+
+procedure generarListaTotalVentasPorFuncion(vectorDias: vectorFunciones; var pi: listaTotalObra);
+    procedure insertarAtras(var pi, pf: listaTotalObra; obra: entradasVendidasObra);
+    var
+        nuevo: listaTotalObra;
+    begin
+        new(nuevo);
+        nuevo^.sig := nil;
+        nuevo^.dato := obra;
+
+        if (pi = nil) then begin
+            pi := nuevo;
+            pf := nuevo;
+        end;
+        else
+            pf^.sig := nuevo;
+            pf := nuevo;
+    end;
+    procedure buscarMinimo(v: vectorDias; oMin: entradasVendidasObra);
+    var
+        i: integer;
+        iMin: integer;
+    begin   
+        oMin.codigoObra := 9999;
+
+        for i := 1 to diasSemana do begin
+            if (v[i] <> nil) then
+                if (oMin.codigoObra < v[i]^.dato.codigoObra) then begin
+                    oMin.codigoObra := v[i]^.dato.codigoObra;
+                    oMin.cantidadVendida := v[i]^.dato.cantidadVendida;
+                    iMin := i;
+            end;
+        end;
+
+        if (oMin.codigoObra <> 9999) then
+            v[iMin] := v[iMin]^.sig
+
+
+    end;
+
+
+var
+    pf: listaTotalObra;
+    obraMin: entradasVendidasObra;
+    obraActual: entradasVendidasObra;
+begin
+    pi := nil;
+    pf := nil;
+
+    buscarMinimo(vectorDias, obraMin);
+
+    while (obraMin.codigoObra <> 9999) do begin
+        obraActual.codigoObra := obraMin.codigoObra;
+        obraActual.cantidadVendida := 0; 
+        
+        while (obraMin.codigoObra <> 9999) and (obraMin.codigoObra = obraActual.codigoObra) do begin
+            obraActual.cantidadVendida := obraActual + obraMin.cantidadVendida;
+            buscarMinimo(vectorDias, obraMin);
+        end;
+
+        insertarAtras(pi, pf, obraActual);
+    end; 
 end;
 
 
 var
+    listasEntradasVendidasPorDia: vectorFunciones;
+    listaTotalObras: listaTotalObra;
 begin
+    generarListaEntradasVendidas(listasEntradasVendidasPorDia);
+    generarListaTotalVentasPorFuncion(listasEntradasVendidasPorDia, listaTotalObras);
 
 end.

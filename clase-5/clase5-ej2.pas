f30a56a3612d015d857d3603817e5bff50897d7b
daniNKV <nikiforovdaniel@gmail.com>
2022-09-12 12:23:11 -0300

    Repaso Ej 2: A B y D (falta C)

--- clase-5/clase5-ej2.pas
+++ clase-5/clase5-ej2.pas
@@ -13,3 +13,144 @@
 // d) Contenga un módulo que reciba el árbol generado en a) y una patente y
 // devuelva el año de fabricación del auto con dicha patente. Mostrar el
 // resultado.
+
+program ej2;
+
+type
+    rangoAntiguedad = 2010 .. 2018;
+
+    arbolStock = ^nodoArbol;
+    listaStockPorAnio = ^nodoLista;
+
+    tipoAuto = record
+        patente: integer;
+        fabricacion: integer;
+        marca: string;
+        modelo: string
+    end;
+
+    nodoArbol = record
+        dato: tipoAuto;
+        hi: arbolStock;
+        hd: arbolStock;
+    end;
+
+    nodoLista = record
+        dato: tipoAuto;
+        sig: listaStockPorAnio;
+    end;
+
+    vectorStockPorAnio = array[rangoAntiguedad] of listaStockPorAnio;
+
+
+
+procedure generarArbol(var a: arbolStock);
+    procedure leerAuto(var a: tipoAuto);
+    begin
+        write('Ingrese patente identificatoria: ');
+        readln(a.patente);
+        if (a.patente <> -1) then begin
+            write('Ingrese año de fabricacion: ');
+            readln(a.fabricacion);
+            write('Ingrese marca del auto: ');
+            readln(a.marca);
+            write('Ingrese modelo del auto: ');
+            readln(a.modelo);
+        end;
+    end;
+
+    procedure insertar(var a: arbolStock; auto: tipoAuto);
+    var
+        nuevo: arbolStock;
+    begin
+        if (a = nil) then begin
+            new(nuevo);
+            nuevo^.dato := auto;
+            nuevo^.hi := nil;
+            nuevo^.hd := nil;
+
+            a := nuevo;
+        end
+        else
+            if (auto.patente > a^.dato.patente) then
+                insertar(a^.hd, auto)
+            else
+                insertar(a^.hi, auto)
+    end;
+
+var
+    auto: tipoAuto;
+begin
+    a := nil;
+
+    leerAuto(auto);
+
+    while (auto.patente <> -1) do begin
+        insertar(a, auto);
+        leerAuto(auto);
+    end;
+end;
+
+procedure calcularUnidadesMarca(a: arbolStock);
+    function cantidadAutos(var a: arbolStock; marca: string):integer;
+    begin
+        if (a = nil) then
+            cantidadAutos := 0
+        else
+            if (marca = a^.dato.marca) then
+                cantidadAutos := 1 + cantidadAutos(a^.hd, marca) + cantidadAutos(a^.hi, marca)
+            else
+                cantidadAutos := cantidadAutos(a^.hd, marca) + cantidadAutos(a^.hi, marca);
+    end;
+var
+    marcaBuscada: string;
+begin
+    write('Ingrese la marca que desea buscar: ');
+    readln(marcaBuscada);
+
+    writeln('La cantidad de autos de la marca ', marcaBuscada, ' es de: ', cantidadAutos(a, marcaBuscada));
+    
+end;
+
+procedure buscarFabricacion(a: arbolStock);
+    function autoConPatente(var a: arbolStock; p: integer): arbolStock;
+    begin
+        if (a = nil) then
+            autoConPatente := nil
+        else
+            if (p = a^.dato.patente) then
+                autoConPatente := a
+            else
+                if (p > a^.dato.patente) then
+                    autoConPatente := autoConPatente(a^.hd, p)
+                else
+                    autoConPatente := autoConPatente(a^.hi, p);
+    end;
+var
+    nodoBuscado: arbolStock;
+    id: integer;
+begin
+    write('Ingrese la patente para la cual desea saber el año de fabricación: ');
+    readln(id);
+
+    nodoBuscado := autoConPatente(a, id);
+
+    if nodoBuscado <> nil then begin
+        writeln;
+        writeln('El año de fabricación para el auto con patente, ', id, ' es el año: ', nodoBuscado^.dato.fabricacion);
+        writeln;
+    end
+    else
+        writeln;
+        writeln('El auto con la patente ingresada no existe');
+        writeln;
+end;
+
+var
+    arbolDeStock: arbolStock;
+
+begin
+    generarArbol(arbolDeStock);
+    calcularUnidadesMarca(arbolDeStock);
+    buscarFabricacion(arbolDeStock);
+end.

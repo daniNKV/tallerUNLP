17c2022b738e834051c2e5660c010fae46b15f38
daniNKV <nikiforovdaniel@gmail.com>
2022-09-28 23:24:25 -0300

    Prepara ej 3

--- Objetos/TP2022_ProyectoAlumnos/src/tema3/Demo01ConstructoresLibro.java
+++ Objetos/TP2022_ProyectoAlumnos/src/tema3/Demo01ConstructoresLibro.java
@@ -15,10 +15,10 @@ public class Demo01ConstructoresLibro {
     public static void main(String[] args) {
         Libro libro1= new  Libro( "Java: A Beginner's Guide",   
                                  "Mcgraw-Hill", 2014,   
-                                 "Herbert Schildt", "978-0071809252", 21.72);
+                                 new Autor("Herbert"), "978-0071809252", 21.72);
         Libro libro2= new Libro("Learning Java by Building Android Games",  
                               "CreateSpace Independent Publishing", 
-                               "John Horton", "978-1512108347");
+                               new Autor("John Horton"), "978-1512108347");
         System.out.println(libro1.toString());
         System.out.println(libro2.toString());
         System.out.println("Precio del libro2: " +libro2.getPrecio());

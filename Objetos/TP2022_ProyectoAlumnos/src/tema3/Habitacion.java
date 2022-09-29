a53217607f67b3b91f23e84013c9f818e50deaf0
daniNKV <nikiforovdaniel@gmail.com>
2022-09-29 18:56:08 -0300

    Creacion Ejercicio 5 y clase Circulo

--- Objetos/TP2022_ProyectoAlumnos/src/tema3/Habitacion.java
+++ Objetos/TP2022_ProyectoAlumnos/src/tema3/Habitacion.java
@@ -3,11 +3,42 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package tema3;
-
+import tema2.Persona;
+import PaqueteLectura.GeneradorAleatorio;
 /**
  *
  * @author dani
  */
 public class Habitacion {
+    private double costo;
+    private boolean empty = true;
+    private Persona cliente;
+    
+    public Habitacion(){
+        GeneradorAleatorio.iniciar();
+        this.setCosto(GeneradorAleatorio.generarDouble(6000) + 2000);
+    }
+    
+    public void setCosto(double precio){
+        this.costo = precio;
+    }
+    public void switchOcupacion(){
+        empty = !(empty = true);
+    }
+    public void setCliente(Persona cliente){
+        this.cliente = cliente;
+    }
+    public void aumentarCosto(double monto){
+        this.costo += monto;
+    }
+    public boolean estaVacia(){
+        return this.empty;
+    }
+    public double getCosto(){
+        return this.costo;
+    }
+    public Persona getCliente(){
+        return this.cliente;
+    }
     
 }
--- Objetos/TP2022_ProyectoAlumnos/src/tema3/Hotel.java
+++ Objetos/TP2022_ProyectoAlumnos/src/tema3/Hotel.java
@@ -4,10 +4,56 @@
  */
 package tema3;
 
+import PaqueteLectura.GeneradorAleatorio;
+import tema2.Persona;
+
 /**
  *
  * @author dani
  */
 public class Hotel {
-    
+
+    private int cantidadHabitaciones;
+    private Habitacion habitaciones[];
+
+    private int getCantidadHabitaciones() {
+        return this.cantidadHabitaciones;
+    }
+
+    public Hotel(int n) {
+        habitaciones = new Habitacion[n];
+    }
+
+    public void ingresarCliente(int nro, Persona cliente) {
+        Habitacion habitacion = this.getHabitacion(nro);
+        if (habitacion.estaVacia()) {
+            habitacion.setCliente(cliente);
+            habitacion.switchOcupacion();
+        } else {
+            System.out.print("La habitación está ocupada");
+        }
+    }
+
+    public void aumentarMontoFijo(double monto) {
+        for (int i = 0; i < this.getCantidadHabitaciones(); i++) {
+            this.getHabitacion(i).aumentarCosto(monto);
+        }
+    }
+
+    private Habitacion getHabitacion(int numeroHabitacion) {
+        return habitaciones[numeroHabitacion];
+    }
+
+    public String toString() {
+        String fullString = "";
+
+        for (int i = 0; i < this.getCantidadHabitaciones(); i++) {
+            Habitacion habitacion = this.getHabitacion(i);
+            String ocupacion = habitacion.estaVacia() ? "Vacia" : "Ocupada";
+            fullString += "{Habitacion: " + i + " costo: " + habitacion.getCosto() + " ocupacion: " + ocupacion;
+        }
+        
+        return fullString;
+    }
+
 }
